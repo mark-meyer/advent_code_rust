0147968cf4206dd3b919d6c74177c00b059208ba
Mark on Laptop <mark@photo-mark.com>
2023-01-02 18:06:11 -0900

    cleanup and refactor a bit

--- 2022/day_08/src/main.rs
+++ 2022/day_08/src/main.rs
@@ -1,20 +1,46 @@
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::collections::HashSet;
 
 static DATA:&str = "data.txt";
 
-fn parse_input(p: &Path) -> Vec<Vec<u32>>{
-    let f = File::open(p).expect("can't read the file");
-    BufReader::new(f)
-    .lines()
-    .map(|line| parse_line(line.unwrap()))
-    .collect()
+type ViewScape = Vec<Option<u32>>;
+struct Forest {
+    rows: Vec<Vec<u32>>,
+    cols: Vec<Vec<u32>>,
+    width: usize,
+    height: usize
 }
-fn parse_line(s: String) -> Vec<u32>{
-    s.chars().flat_map(|c| c.to_digit(10)).collect()
+
+impl Forest {
+    fn new(rows: Vec<Vec<u32>>) -> Forest {
+        Forest {
+            cols: transpose(&rows),
+            width: rows[0].len() ,
+            height: rows.len(),
+            rows: rows,
+        }
+    }
+    fn view_distances(&self, row: usize, column: usize,) -> ViewScape {
+        /*
+        Returns a vector of Options containing the distance until the first blocking
+        tree looking left, right, up, and down.
+        The Option will be None if you can see all the way to the edge
+        */
+        let n = self.rows[row][column];
+        vec![
+            // left
+            index_greater(&n, &mut self.rows[row][..column].iter().rev()),
+            // right
+            index_greater(&n, &mut self.rows[row][column+1..].iter()),
+            // up
+            index_greater(&n, &mut self.cols[column][..row].iter().rev()),
+            // down
+            index_greater(&n, &mut self.cols[column][row+1..].iter())
+        ]
+    }
 }
+
 
 fn transpose(v: &Vec<Vec<u32>>) -> Vec<Vec<u32>> {
     let len = v[0].len();
@@ -30,81 +56,58 @@ fn transpose(v: &Vec<Vec<u32>>) -> Vec<Vec<u32>> {
         .collect()
 }
 
-fn index_taller<'a, I>(tree_height: &u32, view:I) -> Option<usize>
+fn index_greater<'a, I>(tree_height: &u32, view:I) -> Option<u32>
     where
     I: Iterator<Item = &'a u32>
+    /*
+    Returns the index of the first item in the iterator greater or
+    equal to tree_height. Returns None if there are no items greater or equal
+    */
 {
     view
     .enumerate()
     .filter(|(_, n)| *n >= tree_height)
-    .map(|(i, _)| i + 1)
+    .map(|(i, _)| (i + 1) as u32)
     .next()
 }
 
-fn part_one(forest: &Vec<Vec<u32>>) -> usize {
-    let mut visible:HashSet<(usize, usize)> = HashSet::new();
 
-    let height = forest.len();
-    let width = forest[0].len();
+fn parse_input(p: &Path) -> Vec<Vec<u32>>{
+    let f = File::open(p).expect("can't read the file");
+    BufReader::new(f)
+    .lines()
+    .map(|line| parse_line(line.unwrap()))
+    .collect()
+}
 
-    // Forward - find cummulative max moving left-to-right in rows
-    // and top-to-bottom in columns
-    let mut column_max = forest[0].clone();
-    for (row, line) in forest[1..height-1].iter().enumerate() {
-        let mut row_max = line[0];
-        for (column, n) in line[1..width-1].iter().enumerate() {
-            if *n as u32 > row_max {
-                row_max = *n;
-                visible.insert((row+1, column+1));
-            }
-            if *n as u32 > column_max[column+1] {
-                column_max[column+1] = *n;
-                visible.insert((row+1, column+1));
-            }
-        }
-    }
+fn parse_line(s: String) -> Vec<u32>{
+    s.chars().flat_map(|c| c.to_digit(10)).collect()
+}
 
-    // Backward the oppoite
-    let mut column_max = forest[height-1].clone();
-    for (row, line) in forest[1..height-1].iter().enumerate().rev() {
-        let mut row_max = line[width-1];
-
-        for (column, n) in line[1..width-1].iter().enumerate().rev() {
-            if *n as u32 > row_max {
-                row_max = *n;
-                visible.insert((row+1, column+1));
-            }
-            if *n as u32 > column_max[column+1] {
-                column_max[column+1] = *n;
-                visible.insert((row+1, column+1));
+fn part_one(forest: &Forest) -> u32 {
+    let mut total = 0;
+    for row in 0..forest.height {
+        for column in 0..forest.width {
+            let viewscape = forest.view_distances(row, column);
+            if viewscape.iter().any(|o| o.is_none()) {
+                total+=1
             }
         }
     }
-    let outside = 2 * height + 2 * width - 4; // don't count corners twice
-    visible.len()  + outside
-
+    total
 }
 
 
-fn part_two(forest: &Vec<Vec<u32>>) -> u32 {
-    let width = forest[0].len();
-    let height = forest.len();
-    let cols = transpose(&forest);
+fn part_two(forest: &Forest) -> u32 {
     let mut max = 0;
-    for (row, line) in forest.iter().enumerate() {
-        for (column, n) in line.iter().enumerate() {
-            let col = &cols[column];
+    for row in 0..forest.height {
+        for column in 0..forest.width {
+            let viewscape = forest.view_distances(row, column);
+            let total = viewscape[0].unwrap_or(column as u32)
+                * viewscape[1].unwrap_or((forest.width - column - 1) as u32)
+                * viewscape[2].unwrap_or(row as u32)
+                * viewscape[3].unwrap_or((forest.height - row - 1) as u32);
 
-            let taller_right = index_taller(n, &mut line[..column]
-                .iter().rev()).unwrap_or(column);
-            let taller_left = index_taller(n, &mut line[column+1..]
-                .iter()).unwrap_or(width - column - 1);
-            let taller_up = index_taller(n, &mut col[..row]
-                .iter().rev()).unwrap_or(row);
-            let taller_down = index_taller(n, &mut col[row+1..]
-                .iter()).unwrap_or(height - row - 1);
-
-            let total = taller_right * taller_left * taller_up * taller_down;
             if total > max {
                 max = total
             }
@@ -116,8 +119,8 @@ fn part_two(forest: &Vec<Vec<u32>>) -> u32 {
 fn main() {
     let path = Path::new(DATA);
     let lines:Vec<Vec<u32>>  = parse_input(path);
-
-    println!("Part One: {}", part_one(&lines));
-    println!("Part Two: {}", part_two(&lines));
+    let forest = Forest::new(lines);
+    println!("Part One: {}", part_one(&forest));
+    println!("Part Two: {}", part_two(&forest));
 }
 
