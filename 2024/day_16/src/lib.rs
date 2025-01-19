7d53a45680c07e39762cd7895c088c1e91e0f55c
Mark Meyer <mark.meyer@gsa.gov>
2025-01-18 21:57:34 -0500

    improve speed by not saving paths. Calculate the good seats after dijkstra

--- 2024/day_16/src/lib.rs
+++ 2024/day_16/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::BinaryHeap;
 use std::cmp::Ordering;
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
@@ -8,17 +8,24 @@ pub enum Direction {
     South = 2,
     West = 3
 }
-const DIRECTIONS:[Direction;4] = [Direction::North, Direction::East, Direction::South, Direction::West];
+impl Direction {
+    pub fn opposite(&self) -> Self {
+        match self {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::East => Direction::West,
+            Direction::West => Direction::East
+        }
+    }
+}
+pub const DIRECTIONS:[Direction;4] = [Direction::North, Direction::East, Direction::South, Direction::West];
 
 /* 
     HeapNode
-    A struct that implements Ord 
-    to be used in a min heap.
-    It also does some housekeeping to track the path
-    used up to this point for part two.
+    A struct that implements Ord to be used in a min heap.
  */
 #[derive(PartialEq, Eq)]
-struct HeapNode {cost:u64, point:Point, path:Vec<(usize, usize)>}
+struct HeapNode {cost:u64, point:Point}
 
 impl Ord for HeapNode {
     fn cmp(&self, other:&HeapNode) -> Ordering {
@@ -33,15 +40,13 @@ impl PartialOrd for HeapNode {
 
 /*
     MazeSpot
-    Represents each position in the
-    maze along with the lowest cost 
-    so for for each direction on that
-    point. 
+    Represents each position in the maze along with the lowest cost 
+    so for for each direction on that point. 
 */
 #[derive(Clone, Copy, Debug)]
 pub enum MazeSpot {
     Wall,
-    Space, // min cost seen so far
+    Space,
 }
 
 pub struct Maze {
@@ -55,67 +60,67 @@ impl Maze {
         &self.matrix[p.row][p.col]
     }
 
-    pub fn least_cost(&mut self) -> Option<(u64, usize)> {
-        // Keeps track of the min seen in the maze
-        // which means this mutates the maze
-        let start_node = HeapNode{cost:0, point:self.start, path:vec![self.start.coords()]};
+    pub fn least_cost(&self) -> Option<(u64, Vec<Vec<[u64;4]>>)> {        
+        let start_node = HeapNode{cost:0, point:self.start};
         let mut heap:BinaryHeap<HeapNode> = BinaryHeap::from([start_node]);
+
         let mut costs = vec![vec![[u64::MAX; 4]; self.matrix[0].len()]; self.matrix.len()];
 
         let mut global_min = u64::MAX;
-        let mut track_positions = HashSet::new();
 
         while !heap.is_empty() {
-            if let Some(HeapNode{cost, point, path}) = heap.pop() {
+            if let Some(HeapNode{cost, point}) = heap.pop() {
                 let (row, col) = point.coords();
+                let current_direction = point.dir; 
 
+                if cost <=  costs[row][col][current_direction as usize] {
+                    costs[row][col][current_direction as usize] = cost;
+                }
+
+                // Don't return immediately on end Wait until other paths
+                //  of the same cost arrive
                 if (row, col) == self.end {
                     if cost <= global_min {
-                        track_positions.extend(path);
                         global_min = cost;
                     } else {
-                        return Some((global_min, track_positions.len()))
+                        return Some((global_min, costs))
                     }
                     continue
                 }
                 
-                let current_direction = point.dir;    
                 for direction in DIRECTIONS {
                     let neighbor_point = point.step(direction);
-                    let  neighbor_spot = self.get(&neighbor_point);
 
-                    match neighbor_spot {
+                    match self.get(&neighbor_point) {
                         MazeSpot::Wall => {},
                         
                         MazeSpot::Space  => {
                             let previous_cost = costs[neighbor_point.row][neighbor_point.col][direction as usize];
-                            let mut next_cost = cost;
+                            let next_cost;
                             let next_point;
-                            let mut new_path = path.clone();
 
                             if direction != current_direction {
-                                next_cost += 1000;
+                                next_cost = cost + 1000;
                                 next_point = point;
-                                
                             } else {
-                                next_cost += 1;
+                                next_cost = cost +  1;
                                 next_point = neighbor_point;
-                                new_path.push(next_point.coords())
                             }
                             if next_cost <= previous_cost {
                                 heap.push(HeapNode{
                                     cost:next_cost, 
-                                    point:Point{ dir:direction, ..next_point},
-                                    path: new_path
+                                    point:Point{dir:direction, ..next_point},
                                 });
-                                let (row, col) = next_point.coords();
-                                costs[row][col][direction as usize] = next_cost
                             }
                         }
                     }
                 }
             }
         }
+
+        if global_min < u64::MAX {
+            return Some((global_min, costs))
+        } 
         None
     }
 }
--- 2024/day_16/src/main.rs
+++ 2024/day_16/src/main.rs
@@ -1,12 +1,55 @@
 use std::fs;
+use std::collections::HashSet;
 use day_16::*;
 
 
+fn count_paths(end: (usize, usize), costs:&Vec<Vec<[u64;4]>>) -> usize  {
+    // work backward to find valid paths based on 
+    // the costs saved from part one.
+
+    let mut points:HashSet<(usize, usize)> = HashSet::new();
+    let final_cost = costs[end.0][end.1].iter().min().unwrap();
+    
+    let mut stack:Vec<(u64, Point)> = DIRECTIONS.iter()
+        .map(|&d| (*final_cost, Point{row:end.0, col:end.1, dir:d}))
+        .collect();
+
+    while !stack.is_empty() {
+        let (target_cost, point) = stack.pop().unwrap();
+        points.insert((point.row, point.col));
+
+        if target_cost == 0 { // back at start
+            continue
+        }
+
+        for direction in DIRECTIONS {
+            let next = point.step(direction);
+    
+            let travel_cost = costs[next.row][next.col][direction.opposite() as usize];
+
+            if direction == point.dir {   
+                // move to the next spot             
+                if travel_cost < u64::MAX && travel_cost + 1 == target_cost {
+                    stack.push((target_cost - 1, point.step(direction)));
+                } 
+            } else {
+                // turn in place, but don't move
+                let travel_cost = costs[point.row][point.col][direction.opposite() as usize];             
+                if travel_cost < u64::MAX && travel_cost + 1000 == target_cost  {
+                    stack.push((target_cost - 1000, Point{dir: direction, ..point}));
+                }
+            }
+        }
+    }
+    return points.len()
+}
 fn main() {
     let raw_map = fs::read_to_string("data.txt").expect("The elves lost the course map!");
-    let mut maze:Maze = raw_map.into();
-    if let Some((cost, len)) = maze.least_cost() {
+    let maze:Maze = raw_map.into();
+    if let Some((cost, costs)) = maze.least_cost() {
         println!("Part one: {}", cost);
-        println!("Part Two: {}", len);
+    
+        let paths = count_paths(maze.end, &costs);
+        println!{"Part Two: {}", paths};    
     }
 }
