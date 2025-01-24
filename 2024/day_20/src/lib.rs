1bcd5f081102071163c78cf4581a1c4e6a1f43fe
Mark Meyer <mark.meyer@gsa.gov>
2025-01-24 17:58:24 -0500

    fix tree bug

--- 2024/day_20/src/lib.rs
+++ 2024/day_20/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::{VecDeque, HashSet};
 
+pub mod kdtree;
+
 pub enum Direction {
     North,
     East,
@@ -131,7 +133,7 @@ pub struct KdTreeNode<T, const D: usize> {
 }
 
 impl<T, const D: usize> KdTree<T, D> 
-    where  T: PartialOrd + Copy
+    where  T: PartialOrd + Copy + std::fmt::Debug
 {
     pub fn insert(&mut self, value: [T; D]) {
         let mut d = 0;
@@ -152,6 +154,22 @@ impl<T, const D: usize> KdTree<T, D>
             }))
     }
 
+    pub fn values(&self) -> Vec<[T;D]> {
+        let mut stack = vec![&self.root];
+        let mut res = vec![];
+        while !stack.is_empty() {
+            match stack.pop().unwrap() {
+                Some(next) => {
+                    res.push(next.value);
+                    stack.push(&next.left);
+                    stack.push(&next.right);
+                },
+                None => {}
+            }
+        }
+        res
+    }
+
     pub fn find(&self, value: [T; D]) -> Option<&KdTreeNode<T, D>> {
         let mut current = &self.root;
         let mut d = 0;
@@ -170,32 +188,29 @@ impl<T, const D: usize> KdTree<T, D>
     }
     pub fn range_query(&self, min: [T; D], max: [T; D]) -> Vec<[T; D]> {
         let mut result = Vec::new();
-        let mut stack = vec![&self.root]; 
-        let mut d = 0;
+        let mut stack = vec![(&self.root, 0)]; 
     
-        while let Some(current) = stack.pop() {
-            if let Some(node) = current {
+        while !stack.is_empty() {
+            if let (Some(node), d) = stack.pop().unwrap() {
                 let mut in_range = true;
                 for i in 0..D {
-                    if node.value[i] < min[i] || node.value[i] > max[i] {
+                    if node.value[i] < min[i] || node.value[i] > (max[i]) {
                         in_range = false;
                         break;
                     }
                 }
-                if in_range {
+                if in_range {                    
                     result.push(node.value);
                 }
     
-                d = (d + 1) % D;
                 if min[d] <= node.value[d] {
-                    stack.push(&node.left);
+                    stack.push((&node.left, (d + 1) % D));
                 }
                 if max[d] >= node.value[d] {
-                    stack.push(&node.right);
+                    stack.push((&node.right, (d + 1) % D));
                 }
             }
         }
-    
         result
     }
 } 
@@ -211,20 +226,28 @@ mod test {
         kd.insert([4, 4]);
         kd.insert([3, 2]);
         assert_eq!(&kd.root.as_ref().unwrap().value, &[2, 3] );
-        println!("kd: {:?}", &kd);
         assert_eq!(&kd.root.unwrap().right.unwrap().left.unwrap().value, &[3, 2])
     }
     #[test]
     fn test_kd_range() {
         let mut kd = KdTree{root:None};
-        kd.insert([1, 1]);
-        kd.insert([2, 4]);
-        kd.insert([4, 3]);
-        kd.insert([4, 4]);
-        kd.insert([5,5]);
+        kd.insert([6, -4, 2]);
 
-        let res = kd.range_query([2, 2], [4,4]);
-        assert_eq!(res, vec![[2,4], [4,3], [4,3]])
+        //let res = kd.range_query([2, 2], [4,4]);
+        //assert_eq!(res, vec![[2,4], [4,3], [4,3]]);
+
+        let res = kd.range_query([2, -4, -10], [6,0, 20]);
+        assert_eq!(res, vec![[6,-4, 2]])
+    }
+    #[test]
+    fn test_values() {
+        let mut kd = KdTree{root:None};
+        kd.insert([2, 3]);
+        kd.insert([4, 4]);
+        kd.insert([3, 2]);
+        let res = kd.values();
+        assert_eq!(res, [[2,3], [4,4], [3,2]])
+    
     }
 }
 
--- 2024/day_20/src/main.rs
+++ 2024/day_20/src/main.rs
@@ -3,27 +3,26 @@ use std::cmp::max;
 use day_20::*;
 
 /// points are (u,v) points to skew them to manhattan distance is axis aligned.
-fn find_cheats(tree:&KdTree<isize, 3>, uv_points:&[((isize, isize), usize)], cheat_length: isize, min_savings:usize) -> usize{
+fn find_cheats(tree:&KdTree<isize, 3>, cheat_length: isize, min_savings:usize) -> usize{
     let mut total = 0;
 
-    for &((u, v), start_dist) in uv_points {
-
+    for &[u, v, start_dist] in tree.values().iter() {
+        
         for  &[u_next, v_next, end_dist] in &tree.range_query(
-                [u - cheat_length , v - cheat_length , 0 as isize ],
-                [u + cheat_length + 1, v + cheat_length + 1, 1000000]) {
-            let cheat = max(v.abs_diff(v_next), u.abs_diff(u_next)) as isize;   // manhattan distance between the two points
-            //println!("{}", end_dist.abs_diff(start_dist as isize) - (cheat as usize) );
-            //if cheat <= cheat_length                                   // don't go through too many walls
-            if (start_dist as isize) + cheat < end_dist                           // the cheat is actually an improvement
-            && end_dist.abs_diff(start_dist as isize) - (cheat as usize) >= min_savings {  // avoid counting tiny savings
-                println!("cheat: {}", end_dist.abs_diff(start_dist as isize) - (cheat as usize));
+                [u - cheat_length , v - cheat_length , 200 as isize ],
+                [u + cheat_length, v + cheat_length , 10000]) {
 
+            let cheat = max(v.abs_diff(v_next), u.abs_diff(u_next)) as isize;              // manhattan distance between the two points
+            
+            if (start_dist as isize) + cheat < end_dist                                    // is the cheat is actually an improvement?
+            && end_dist.abs_diff(start_dist as isize) - (cheat as usize) >= min_savings {  // avoid counting small savings
                 total += 1;
             } 
         }
     }
     total
 }
+
 fn main() {
     let s = fs::read_to_string("data.txt").expect("no map found?");
     let mut map = Map::from(&s);
@@ -36,10 +35,11 @@ fn main() {
 
     
     uv_path.sort_by_key(|((u, _v), _dist)| *u);
+    println!("path length: {}", uv_path.len());
     
-    let part_one = find_cheats(&kdtree, &uv_path, 2, 0);
+    let part_one = find_cheats(&kdtree, 2, 100);
     println!("Part one: {}", part_one);
 
-    // let part_two = find_cheats(&uv_path, 20, 100);
-    // println!("Part two: {}", part_two);
+    let part_two = find_cheats(&kdtree, 20, 100);
+    println!("Part two: {}", part_two);
 }
