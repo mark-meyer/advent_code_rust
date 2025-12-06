cc9c5fb35025705f38242163bf1794122a360240
Mark Meyer <mark.meyer@gsa.gov>
2025-12-06 18:47:22 -0500

    some minor style improvements day 6

--- 2025/day_06/src/main.rs
+++ 2025/day_06/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use std::str::FromStr;
 
 #[derive(Debug)]
 enum Operator {
@@ -6,18 +7,20 @@ enum Operator {
     Product
 }
 
-impl From<&str> for Operator {
-   fn from(op: &str) -> Operator {
+impl FromStr for Operator {
+    type Err = String;
+    fn from_str(op: &str) -> Result<Operator, Self::Err> {
+
         match op {
-            "+" => Operator::Sum,
-            "*" => Operator::Product,
-            _ => panic!("Non-cephalopod operator found!")
+            "+" => Ok(Operator::Sum),
+            "*" => Ok(Operator::Product),
+            _ => Err(format!("Non-cephalopod operator found! {}", op))
         }
    } 
 }
 
 fn parse_file_two(raw_data: &str) -> Vec<(Operator, Vec<u64>)>{
-    let lines: Vec<Vec<char>> = raw_data.split("\n").map(|l| l.chars().collect()).collect();
+    let lines: Vec<Vec<char>> = raw_data.lines().map(|l| l.chars().collect()).collect();
     let rows = lines.len();
     let cols = lines[0].len();
     let ops = lines[rows-1].iter().filter(|c| !c.is_ascii_whitespace());
@@ -30,21 +33,23 @@ fn parse_file_two(raw_data: &str) -> Vec<(Operator, Vec<u64>)>{
     });
 
     ops.map(|op| {
-        let operator = Operator::from(op.to_string().as_str());
+        let mut tmp = [0; 4];
+        let s = op.encode_utf8(&mut tmp);
+        let operator = s.parse().expect("Bad Operator");
 
         let nums:Vec<u64> = n_gen.by_ref()
-            .take_while(|s| s != "")
-            .map(|s| s.parse::<u64>().unwrap())
+            .take_while(|s| !s.is_empty())
+            .map(|s| s.parse().unwrap())
             .collect();
-        
+
         (operator, nums)
     })
     .collect()
 }
 
 fn parse_file_one(raw_data: &str) -> Vec<(Operator, Vec<u64>)> {
-    let lines: Vec<Vec<&str>> = raw_data.split("\n")
-    .map(|line| line.split_ascii_whitespace().collect())
+    let lines: Vec<Vec<&str>> = raw_data.lines()
+    .map(|line| line.split_whitespace().collect())
     .collect();
 
     let rows = lines.len();
@@ -57,13 +62,13 @@ fn parse_file_one(raw_data: &str) -> Vec<(Operator, Vec<u64>)> {
     let ops = &lines[rows-1];
 
     ops.into_iter().map(|&op| {
-        let operator = Operator::from(op);
+        let operator = op.parse().expect("Invalid Operator");
         (operator, n_gen.next().unwrap())
     })
     .collect()
 }
 
-fn cephalopod_math(input: Vec<(Operator,  Vec<u64>)>) -> u64 {
+fn cephalopod_math(input: &[(Operator,  Vec<u64>)]) -> u64 {
     input.iter()
     .map(|(operator, nums)| match operator {
         Operator::Sum => nums.iter().sum::<u64>(),
@@ -75,9 +80,9 @@ fn cephalopod_math(input: Vec<(Operator,  Vec<u64>)>) -> u64 {
 fn main() {
     let s = read_to_string("data.txt").expect("could not open file");
     
-    let answer_one = cephalopod_math(parse_file_one(&s));
+    let answer_one = cephalopod_math(&parse_file_one(&s));
     println!("Part One: {}", answer_one);
 
-    let answer_two = cephalopod_math(parse_file_two(&s));
-    println!("Part One: {}", answer_two);
+    let answer_two = cephalopod_math(&parse_file_two(&s));
+    println!("Part Two: {}", answer_two);
 }
