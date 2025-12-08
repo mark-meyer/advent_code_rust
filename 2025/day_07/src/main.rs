106ba57a9f5a691b1e5816340ba220af594d5487
Mark Meyer <mark.meyer@gsa.gov>
2025-12-07 19:26:46 -0500

    simplify DP

--- 2025/day_07/src/main.rs
+++ 2025/day_07/src/main.rs
@@ -1,9 +1,10 @@
 use std::fs::read_to_string;
+use std::collections::HashSet;
 
 #[derive(Debug, Clone, Copy)]
 enum Cell {
-    Beam(u64),
-    Splitter(bool),
+    Beam,
+    Splitter,
     Space
 }
 
@@ -11,8 +12,8 @@ type Manifold = Vec<Vec<Cell>>;
 
 fn parse_file(s:String) -> Manifold {
     s.lines().map(|line| line.chars().map(|c| match c {
-        '^' => Cell::Splitter(false),
-        'S' => Cell::Beam(1),
+        '^' => Cell::Splitter,
+        'S' => Cell::Beam,
         '.' => Cell::Space,
         _ => panic!("There's a bug in the tachyon manifold. Shut it down!")
         }).collect()
@@ -20,76 +21,45 @@ fn parse_file(s:String) -> Manifold {
     .collect()
 }
 
+fn run_transporter_two(m: &Manifold) -> (u64, usize) {
+    let h = m.len();
+    let w = m[0].len();
+    // Keep track of used spliters
+    let mut used_splitters = HashSet::new();
 
-fn run_transporter(m: &Manifold) -> Manifold {
-    let mut record = m.clone();
-    let h = record.len();
-    let w = record[0].len();
+    // DP record
+    let mut record = vec![vec![0; w]; h];
+    let start = m[0].iter().position(|c| matches!(c, Cell::Beam)).unwrap();
 
-    for row in 0..h-1 {
-        for col in 0..w {
-            if let Cell::Beam(count) = record[row][col] {
-                match record[row+1][col] {
-                    Cell::Splitter(_) => {
-                        match record[row+1][col-1] {
-                            Cell::Beam(prev_count) => record[row+1][col-1] = Cell::Beam(count + prev_count),
-                            Cell::Space => record[row+1][col-1] = Cell::Beam(count),
-                            Cell::Splitter(_) => ()
-                        }
-                        match record[row+1][col+1] {
-                            Cell::Beam(prev_count) => record[row+1][col+1] = Cell::Beam(count + prev_count),
-                            Cell::Space => record[row+1][col+1] = Cell::Beam(count),
-                            Cell::Splitter(_) => ()
-                        }
-                        record[row+1][col] = Cell::Splitter(true);
-                    }
-                    Cell::Beam(prev_count) => {
-                        record[row+1][col] = Cell::Beam(count+prev_count);
-                    }
-                    Cell::Space => { 
-                        record[row+1][col] = Cell::Beam(count) 
+    record[0][start] = 1;
+
+    for (row, line) in m.iter().enumerate().skip(1) {
+        for (col, cell) in line.iter().enumerate() {
+            
+            match cell {
+                Cell::Space => record[row][col] = record[row-1][col] + record[row][col],
+                Cell::Beam => (),
+                Cell::Splitter=> {
+                    record[row][col-1] = record[row][col-1] + record[row-1][col];
+                    record[row][col+1] = record[row][col+1] + record[row-1][col];
+                    if record[row-1][col] > 0 {
+                        used_splitters.insert((row, col));
                     }
                 }
             }
         }
     }
-    record
+    (record[h-1].iter().sum(), used_splitters.len())
+
 }
 
-fn count_activated_splitters(m: &Manifold) -> u64 {
-    let mut count = 0;
-    for row in m.iter(){
-        for c in row.iter() {
-            if let Cell::Splitter(true) = c {
-                count += 1;
-            }
-        }
-    }
-    count
-}
-
-fn count_paths(m: &Manifold) -> u64 {
-    let last_row:&Vec<Cell> = m.iter().last().unwrap();
-
-    last_row.iter()
-    .fold(0, |acc, cell| {
-        if let Cell::Beam(count) = cell {
-            count + acc
-        } else {
-            acc
-        }
-    })
-  
-}
 
 fn main() {
     let raw_input = read_to_string("data.txt").expect("Couldn't start the tachyon emiiter");
     let manifold = parse_file(raw_input);
 
-    let record = run_transporter(&manifold);
-    let splits = count_activated_splitters(&record);
-    let paths = count_paths(&record);
-    println!("Part one: {}", splits);
-    println!("Part two {}", paths);
 
+    let (paths, splits) = run_transporter_two(&manifold);
+    println!("Part one: {}", splits);
+    println!("Part two: {}", paths);
 }
